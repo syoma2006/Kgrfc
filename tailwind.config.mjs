20a393ff56c47f756114d336737ca310ea457846
syoma2006 <syoma2006@icloud.com>
2026-06-16 11:42:04 +0900

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,7 @@
 
 const config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
